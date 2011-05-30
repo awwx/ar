976f47b0fc731f7e4d01a3e8ccdbc1a50aecfea0
Andrew Wilcox <andrew.wilcox@gmail.com>
2011-05-30 12:37:12 -0700

    use the "arc" script for running tests

---
+++ io-test.sh
@@ -0,0 +1,1 @@
+./arc --no-repl io.arc equal-wrt-testing.arc test.arc io.t
--- tests.sh
+++ tests.sh
@@ -2,8 +2,8 @@
 set -e -v
 ./arc --no-repl ar-test.arc
 ./arc --no-repl ac-test.arc
-racket arc-test.ss
-racket io-test.ss
-sudo `which racket` io-test-root.ss
-racket strings-test.ss
+./arc-test.sh
+./io-test.sh
+./io-test-root.sh
+./strings-test.sh
 ./arc-script-test.pl
